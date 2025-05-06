904d706f49fee5b1e8820e1c43728e386feb30e9
PatriStelmach <patrzy108@gmail.com>
2025-05-06 19:18:49 +0200

    no tests

--- src/main/java/org/example/Flight.java
+++ src/main/java/org/example/Flight.java
@@ -5,16 +5,13 @@ public class Flight
     private long fromInclusive;
     private long toExclusive;
     private short seats;
-    private boolean areSeatsChanged = false;
-    private Period period;
     private long allPassengers = 0;
-
 
     Flight(long from,short seats)
     {
         this.fromInclusive = from;
         this.seats = seats;
-        this.toExclusive = 100000000000L;
+        this.toExclusive = 100_000_000_001L; // 0 ≤ t ≤10^11
     }
     //P
     public void addNewPeriod(long from, short newPeriodSeats)
@@ -22,56 +19,30 @@ public class Flight
         allPassengers = (from - fromInclusive) * seats;
         fromInclusive = from;
         seats = newPeriodSeats;
-        setPeriodChanged(true);
-
     }
 
-    public long getFromInclusive() {
+    public long getFromInclusive()
+    {
         return fromInclusive;
     }
 
-    public long getToExclusive() {
+    public long getToExclusive()
+    {
         return toExclusive;
     }
 
     public void setToExclusive(long toExclusive) {
         this.toExclusive = toExclusive;
     }
-    public void setFromInclusive(long toExclusive) {
-        this.fromInclusive = fromInclusive;
-    }
 
     public short getSeats()
     {
         return seats;
     }
-    public void setSeats(short seats)
+
+    public long getAllPassengers()
     {
-        this.seats = seats;
-    }
-
-
-    public boolean isPeriodChanged() {
-        return areSeatsChanged;
-    }
-
-    public void setPeriodChanged(boolean areSeatsChanged) {
-        this.areSeatsChanged = areSeatsChanged;
-    }
-
-    public long getAllPassengers() {
         return allPassengers;
     }
 
-    public void setAllPassengers(long allPassengers) {
-        this.allPassengers = allPassengers;
-    }
-
-    public Period getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(Period period) {
-        this.period = period;
-    }
 }
